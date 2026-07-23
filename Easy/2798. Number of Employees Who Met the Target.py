b97687142a6b474c10e18f8cea18124b075f80ff
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-07-24 00:40:57 +0600

    num of employee

---
+++ Easy/2798. Number of Employees Who Met the Target.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def numberOfEmployeesWhoMetTarget(self, hours, target):
+        """
+        :type hours: List[int]
+        :type target: int
+        :rtype: int
+        """
+        count=0
+        for i in hours:
+            if target<=i:
+                count+=1
+        
+        return count
