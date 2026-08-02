43135c380caf513635326f7c55612fd2f97dde0c
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-08-02 22:53:26 +0600

    1662. Check If Two String Arrays are Equivalent

---
+++ String/1662. Check If Two String Arrays are Equivalent.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def arrayStringsAreEqual(self, word1, word2):
+        """
+        :type word1: List[str]
+        :type word2: List[str]
+        :rtype: bool
+        """
+        w1="".join(word1)
+        w2="".join(word2)
+
+        return w1==w2
