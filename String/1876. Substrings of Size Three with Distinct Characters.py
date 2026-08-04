1d7a80ee1dc424c5dc4b2c2b22676cd0a3fb4721
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-08-04 22:24:42 +0600

    1876. Substrings of Size Three with Distinct Characters

---
+++ String/1876. Substrings of Size Three with Distinct Characters.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def countGoodSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        res=0
+        for i in range(len(s)-2):
+            if len(set(s[i:i+3]))==3:
+                res+=1
+        return res
