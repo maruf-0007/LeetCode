45e6349ff7317676c65560176f0f28737916832b
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-08-09 22:38:11 +0600

    1137. N-th Tribonacci Number

---
+++ Math/1137. N-th Tribonacci Number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==0 or n==1:
+            return n
+        if n==2:
+            return 1
+        fib=[0]*(n+1)
+        fib[0]=0
+        fib[1]=1
+        fib[2]=1
+        for i in range(3,n+1):
+            fib[i]=fib[i-1]+fib[i-2]+fib[i-3]
+        return fib[n]
