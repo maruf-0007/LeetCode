61c81c6c19b18aaae57be046251536ab8a8e6329
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-08-10 22:19:03 +0600

    1266. Minimum Time Visiting All Points

---
+++ Math/1266. Minimum Time Visiting All Points.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def minTimeToVisitAllPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        res=0
+        for i in range(1,len(points)):
+            res+=max(abs(points[i][0]-points[i-1][0]),abs(points[i][1]-points[i-1][1]))
+
+        return res
