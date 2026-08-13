1ec5e9626a0b80303a41e4be8469861b81bc2274
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-08-13 22:06:38 +0600

    1925. Count Square Sum Triples

---
+++ Math/1925. Count Square Sum Triples.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def countTriples(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        res=0
+        for i in range(1,n):
+            for j in range(i+1,n):
+                s=math.sqrt(i*i+j*j)
+                if int(s)==s and s<=n:
+                    res+=2
+        return res
