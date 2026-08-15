e3455bbee01cd5829a8319deac06f51e26eb542a
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-08-15 22:21:02 +0600

    2651. Calculate Delayed Arrival Time

---
+++ Math/2651. Calculate Delayed Arrival Time.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def findDelayedArrivalTime(self, arrivalTime, delayedTime):
+        """
+        :type arrivalTime: int
+        :type delayedTime: int
+        :rtype: int
+        """
+        total=arrivalTime+delayedTime
+        return total%24
