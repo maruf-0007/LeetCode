3c2d3fe2700335df58f06a26b2cc870afb9c9c5e
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-08-22 23:31:05 +0600

    374. Guess Number Higher or Lower

---
+++ Binary Search/374. Guess Number Higher or Lower.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def guessNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        l,r=1,n
+        while l<=r:
+            mid=(l+r)//2
+            res=guess(mid)
+            if res==0:
+                return mid
+            elif res==-1:
+                r=mid-1
+            else:
+                l=mid+1
