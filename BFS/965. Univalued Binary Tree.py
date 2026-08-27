9abf83c74c002c6fc6101366f15ce0151e0c4b6e
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-08-27 16:41:22 +0600

    965. Univalued Binary Tree

---
+++ BFS/965. Univalued Binary Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isUnivalTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        if not root:
+            return True
+        if root.right:
+            if root.val!=root.right.val:
+                return False
+        if root.left:
+            if root.val!=root.left.val:
+                return False
+        
+        return self.isUnivalTree(root.right) and self.isUnivalTree(root.left)
