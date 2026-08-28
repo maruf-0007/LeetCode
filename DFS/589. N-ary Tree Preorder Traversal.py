ebe0281869c28208df46f07e7b5dcb6ef117bf67
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-08-29 00:21:30 +0600

    589. N-ary Tree Preorder Traversal

---
+++ DFS/589. N-ary Tree Preorder Traversal.py
@@ -0,0 +1,23 @@
+"""
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution(object):
+    def preorder(self, root):
+        """
+        :type root: Node
+        :rtype: List[int]
+        """
+        res = []
+        def dfs(node):
+            if not node:
+                return
+            res.append(node.val)
+            for i in node.children:
+                dfs(i)
+        dfs(root)
+        return res
