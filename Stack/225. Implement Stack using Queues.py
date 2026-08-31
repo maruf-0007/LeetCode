6fd85549f7f7e18b1b21a23aa683106db4fb8bd4
Md. Maruf Hasan Tuhin <marufh2894@gmail.com>
2026-08-31 20:16:48 +0600

    225. Implement Stack using Queues

---
+++ Stack/225. Implement Stack using Queues.py
@@ -0,0 +1,42 @@
+from collections import deque
+class MyStack(object):
+
+    def __init__(self):
+        self.q=deque()
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        self.q.append(x)
+        qLen=len(self.q)
+        while qLen>1:
+            self.q.append(self.q.popleft())
+            qLen-=1
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        return self.q.popleft()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.q[0]
+
+    def empty(self):
+        """
+        :rtype: bool
+        """
+        return len(self.q)==0
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
